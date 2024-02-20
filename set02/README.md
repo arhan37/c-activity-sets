ea397ceaaa4cd5238f8e2da6222fa47308200dc9
Arhan <arhank.bsc23@rvu.edu.in>
2024-02-20 14:01:08 +0530

    program05

--- set02/README.md
+++ set02/README.md
@@ -70,7 +70,7 @@ void output(int n, int result);
 
 ---
 
-04.  Write a program to find Sum of composite number in an array of different numbers entered by the user.
+04.  1
 
 ***Function Declarations***
 ```c
---
+++ set02/problem05.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int input();
+int find_gcd(int a, int b);
+void output(int a, int b, int gcd);
+
+int main() {
+    int a = input();
+    int b = input();
+    int gcd = find_gcd(a, b);
+    output(a, b, gcd);
+    return 0;
+}
+
+int input() {
+    int num;
+    printf("Please enter an integer: ");
+    scanf("%d", &num);
+    return num;
+}
+
+int find_gcd(int a, int b) {
+    int gcd = 1;
+    for (int i = 1; i <= a && i <= b; i++) {
+        if (a % i == 0 && b % i == 0) {
+            gcd = i;
+        }
+    }
+    return gcd;
+}
+
+void output(int a, int b, int gcd) {
+    printf("The GCD of %d and %d is %d\n", a, b, gcd);
+}
